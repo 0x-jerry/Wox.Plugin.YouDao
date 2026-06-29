16c97b24ddefb6ce857638048563f2db223722f7
Jerry <x.jerry.wang@gmail.com>
2026-06-29 07:26:02 +0000

    chore: auto update plugin manifest

--- scripts/pack.ts
+++ scripts/pack.ts
@@ -1,15 +1,33 @@
 import { $ } from 'bun'
 import path from 'node:path'
 import { zipSync } from 'fflate'
+import { version } from '../package.json'
+
+const pluginName = 'Wox.Plugin.YouDao.wox'
 
 // build
 for await (const line of $`tsdown`.lines()) {
   console.log(line)
 }
 
-const archive = await createZipFile(path.resolve('dist'))
+// update version
+await updatePluginManifest()
 
-await Bun.write(`Wox.Plugin.YouDao.wox`, archive)
+// create zip
+const archive = await createZipFile(path.resolve('dist'))
+await Bun.write(pluginName, archive)
+
+console.log('Create wox plugin to:', pluginName)
+
+async function updatePluginManifest() {
+  const pluginConfigFile = Bun.file('dist/plugin.json')
+
+  const pluginConfig = await pluginConfigFile.json()
+
+  pluginConfig.Version = version
+
+  await pluginConfigFile.write(JSON.stringify(pluginConfig, null, 2))
+}
 
 async function createZipFile(dir: string): Promise<Uint8Array> {
   const files: Record<string, Uint8Array> = {}
